133fb17c5f7150f4e4870334f384ce15e56b49a6
hpc4ai <hpc4ai.com>
2025-10-08 22:31:57 +0800

    week 2: backprop

--- code/handwritten_digits.py
+++ code/handwritten_digits.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mnist_loader
+from typing import Optional
 
 def sigmoid(a):
     return 1 / (1 + np.exp(-a))
@@ -22,22 +23,31 @@ class NetWork():
             a = sigmoid(np.matmul(w, a) + b)
         return a
     
-    def evaluate(self, test_data):
+    def evaluate(
+        self, 
+        test_data: list,
+    ):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
         network's output is assumed to be the index of whichever
         neuron in the final layer has the highest activation."""
-        import pdb
-        pdb.set_trace()
         results = [(np.argmax(self.feed_forward(x)), y) for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in results)
     
-    def cost_derivative(self, output_activations, y):
+    def cost_derivative(
+        self, 
+        output_activations: np.array, 
+        y: np.array
+    ):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
         return (output_activations-y)
 
-    def backprop(self, x, y):
+    def backprop(
+        self, 
+        x: np.array,
+        y: np.array
+    ):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
@@ -49,10 +59,7 @@ class NetWork():
         activations = [x] # list to store all the activations, layer by layer
         zs = [] # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
-            # if np.dot(w, activation).shape[1] != b.shape[1]:
-            # import pdb
-            # pdb.set_trace()
-            z = np.dot(w, activation)+b
+            z = np.matmul(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
@@ -60,7 +67,7 @@ class NetWork():
         delta = self.cost_derivative(activations[-1], y) * \
             sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        nabla_w[-1] = np.matmul(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
         # differently to the notation in Chapter 2 of the book.  Here,
         # l = 1 means the last layer of neurons, l = 2 is the
@@ -70,12 +77,16 @@ class NetWork():
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            delta = np.matmul(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+            nabla_w[-l] = np.matmul(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
     
-    def update_mini_batch(self, mini_batch, eta):
+    def update_mini_batch(
+        self,
+        mini_batch: list,
+        eta: float
+    ):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
@@ -84,15 +95,23 @@ class NetWork():
         nbala_b = [np.zeros_like(bias) for bias in self.biases]
         for x, y in mini_batch:
             # Compute the gradient of the weights and biases with respect to a single data point.
+            # How to compute gradients for a batch all at once?
             delta_nbala_b, delta_nbala_w = self.backprop(x, y)
             nbala_w = [nw+dnw for (nw, dnw) in zip(nbala_w, delta_nbala_w)]
             nbala_b = [nb+dnb for (nb, dnb) in zip(nbala_b, delta_nbala_b)]
         
+        # The gradients here need to be divided by len(mini_batch).
         self.weights = [w - (eta/len(mini_batch))*nw for (w, nw) in zip(self.weights, nbala_w)]
         self.biases = [b - (eta/len(mini_batch))*nb for(b, nb) in zip (self.biases, nbala_b)]
     
-    def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
+    def stochastic_gradient_descent(
+        self, 
+        training_data: list,
+        epochs: int,
+        mini_batch_size: int,
+        eta: float,
+        test_data: Optional[list] = None
+    ):
         """Train the neural network using mini-batch stochastic
         gradient descent. The "training_data" is a list of tuples
         "(x, y)" representing the training inputs and the desired
@@ -102,11 +121,13 @@ class NetWork():
         epoch, and partial progress printed out. This is useful for
         tracking progress, but slows things down substantially."""
 
+        # training_data[][0].shape == (768, 1), training_data[][1].shape == (10, 1)
+        # test_data[][0].shape == (768, 1), test_data[][1].shape == (1)
         n = len(training_data)
         for j in range(epochs):
             np.random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size]
-                    for k in range(0, n, mini_batch_size)]
+                for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
